baa83f9a3895af4927745c54e1d8638a81469a5d
Isabella Wang <wangisa26@7hills.org>
2024-06-06 16:34:46 -0400

    step 3a

--- app.rb
+++ app.rb
@@ -18,22 +18,45 @@ class App < Sinatra::Base
   def title
     'Summer Instititue App'
   end
-
+    
+    def projects_root
+        "#{__dir__}/projects/"
+    end
   get '/examples' do
     erb(:examples)
   end
+    
 
+def sanitizeName(name = "")
+    name.downcase.gsub(" ", "_")
+end
   get '/' do
     logger.info('requsting the index')
     @flash = session.delete(:flash) || { info: 'Welcome to Summer Institute!' }
+    @projectDirect = Dir.children(projects_root).select do |path|
+        Pathname.new("#{projects_root}/#{path}").directory?
+    end.sort_by(@:to_s)
     erb(:index)
   end
   
-  get '/projects/new' do
-    erb(:new_project)
-  end
-  
     post '/projects/new' do
-      "Hello world"
+     
+     name = params[:name].downcase.gsub(" ", "_")
+     FileUtils.mkdir_p("#{projects_root}/#{name}")
+     session[:flash] = {info: "Made the project #{name}"}
+     redirect(url("/projects/#{name}"))
     end
+  get '/projects/:name' do
+    if params[:name] == 'new'
+      erb(:new_project)
+    else
+      @directory = Pathname.new("#{projects_root}/#{params[:name]}")
+      if(@directory.directory? && @directory.readable?)
+        erb(:show_project)
+      else
+        session[:flash] = { danger: "#{@directory} does not exist" }
+        redirect(url('/'))
+      end
+    end
+  end
 end
